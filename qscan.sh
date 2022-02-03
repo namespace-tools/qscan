52fe009ef5b4aa4694c970b50810f04cf8b45ce0
namespace <76843450+namespace-tools@users.noreply.github.com>
2022-02-03 17:54:26 +0000

    Create qscan.sh

---
+++ qscan.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Run standard nmap scans w/ notifications
+
+# Log files:
+INITIAL=nmap.initial
+FULL=nmap.full
+
+if [ -f $INITIAL -o -f $FULL ];
+	then
+		echo "[!] $INITIAL and/or $FULL already exists in $PWD.\n[!] Teminating Scan."
+	else
+		if [ $# -eq 0 ];
+			then
+				echo "[!] Missing target hostname or IP.\n[-] Usage: qscan.sh <target>"
+			else
+				notify-send "Initial Scan Started" "Host: $1"
+				nmap -Pn -sV -oN $INITIAL $1 >/dev/null
+				notify-send "Initial Scan Compelte/Begin Full Scan" "Host: $1"
+				nmap -Pn -sV -oN $FULL $1 >/dev/null
+				notify-send "Full Scan Complete!" "Host: $1"
+		fi
+fi
